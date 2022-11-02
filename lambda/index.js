3409624e1c12d0ecd77fdbf108b3c3c1dfe3db11
EC2 Default User <ec2-user@ip-172-31-41-231.ap-northeast-1.compute.internal>
2022-11-02 07:18:49 +0000

    update cricleci config file

---
+++ lambda/index.js
@@ -0,0 +1,18 @@
+let response;
+
+exports.handler = async (event, context) =>
+{
+    try{
+        response = {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: "Hello AWS!",
+            }),
+        };
+    } catch (err){
+        console.log(err);
+        return err;
+    }
+    
+    return response;
+};
--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -1,16 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const fn = new lambda.Function(this, 'LambdaFunction', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('lambda/')
+    });
   }
 }
