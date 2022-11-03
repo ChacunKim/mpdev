30cacf16aca551acda634d8a4a87daf24b3d4c08
EC2 Default User <ec2-user@ip-172-31-41-231.ap-northeast-1.compute.internal>
2022-11-03 04:08:41 +0000

    update lambda function

--- lambda/index.js
+++ lambda/index.js
@@ -1,28 +1,28 @@
-const {exec} = require("node:child_process");
+const { exec } = require('node:child_process');
 
 let response;
-
-exports.handler = async(event, context) => {
-    try{
-        exec("ls -al", (err, stdout, stderr) =>{
-            if (err) {
-                throw Error('error: ${err.message}');
-            }
-            if (stderr) {
-                console.log('stderr: ${stderr}');
-            }
-            
-            response = {
-                statusCode: 200,
-                body: JSON.stringfy({
-                    message: "Hello Marketplace!",
-                }),
-            };
-        });
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
-    
-    return response;
+ 
+exports.handler = async (event, context) => {
+  try {
+    exec("ls -al", (err, stdout, stderr) => {
+        if (err) {
+            throw Error(`error: ${err.message}`);
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`);
+        }
+        
+      response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: "Hello Marketplace!",
+        }),
+      };
+    });
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+ 
+  return response;
 };
